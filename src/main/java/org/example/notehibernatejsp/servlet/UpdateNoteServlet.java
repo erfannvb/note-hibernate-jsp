89143650b1b1ad351dc2474e5c56a0f423980db1
Erfan Navab <erfannavab@gmail.com>
2023-12-25 19:50:49 +0330

    added update servlet

---
+++ src/main/java/org/example/notehibernatejsp/servlet/UpdateNoteServlet.java
@@ -0,0 +1,55 @@
+package org.example.notehibernatejsp.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.notehibernatejsp.base.repository.util.HibernateUtil;
+import org.example.notehibernatejsp.entity.Note;
+import org.example.notehibernatejsp.repository.NoteRepository;
+import org.example.notehibernatejsp.repository.impl.NoteRepositoryImpl;
+import org.example.notehibernatejsp.service.NoteService;
+import org.example.notehibernatejsp.service.impl.NoteServiceImpl;
+import org.hibernate.Session;
+
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "UpdateNoteServlet", urlPatterns = "/updateNote")
+public class UpdateNoteServlet extends HttpServlet {
+
+    private Session session;
+    private NoteRepository noteRepository;
+    private NoteService noteService;
+
+    @Override
+    public void init() throws ServletException {
+        session = HibernateUtil.getSessionFactory().openSession();
+        noteRepository = new NoteRepositoryImpl(session);
+        noteService = new NoteServiceImpl(session, noteRepository);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+
+            long id = Long.parseLong(req.getParameter("id"));
+            String title = req.getParameter("title");
+            String content = req.getParameter("content");
+
+            Note currentNote = noteService.getNoteById(id);
+
+            currentNote.setTitle(title);
+            currentNote.setContent(content);
+            currentNote.setCreatedDate(new Date());
+
+            noteService.update(currentNote);
+
+            resp.sendRedirect("/allNotes.jsp");
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
