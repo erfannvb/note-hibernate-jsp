ef2fc0ecb3edd25628f5d9acff2c8ffaa54ff565
Erfan Navab <erfannavab@gmail.com>
2023-12-25 19:51:12 +0330

    added html tags

--- src/main/java/org/example/notehibernatejsp/servlet/AddNoteServlet.java
+++ src/main/java/org/example/notehibernatejsp/servlet/AddNoteServlet.java
@@ -43,7 +43,9 @@ public class AddNoteServlet extends HttpServlet {
             resp.setContentType("text/html");
 
             PrintWriter writer = resp.getWriter();
-            writer.println("<h2>Note Saved Successfully!!!</h2>");
+            writer.println("<h2 style='text-align=center'>Note Saved Successfully!!!</h2>");
+            writer.println("<h2 style='text-align=center'><a href='allNotes.jsp'>View All Notes</a></h2>");
+
         } catch (Exception e) {
             e.getStackTrace();
         }
