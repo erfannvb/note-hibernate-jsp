3ab6cc6dda5994dcd841cbfff9cc36bca189dee1
Erfan Navab <erfannavab@gmail.com>
2023-12-25 20:43:45 +0330

    used for each to remove a note by id

--- src/main/java/org/example/notehibernatejsp/servlet/DeleteNoteServlet.java
+++ src/main/java/org/example/notehibernatejsp/servlet/DeleteNoteServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.notehibernatejsp.base.repository.util.HibernateUtil;
+import org.example.notehibernatejsp.entity.Note;
 import org.example.notehibernatejsp.repository.NoteRepository;
 import org.example.notehibernatejsp.repository.impl.NoteRepositoryImpl;
 import org.example.notehibernatejsp.service.NoteService;
@@ -13,6 +14,8 @@ import org.example.notehibernatejsp.service.impl.NoteServiceImpl;
 import org.hibernate.Session;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "DeleteNoteServlet", urlPatterns = "/delete")
 public class DeleteNoteServlet extends HttpServlet {
@@ -33,7 +36,13 @@ public class DeleteNoteServlet extends HttpServlet {
         try {
 
             long id = Long.parseLong(req.getParameter("id"));
-            noteService.deleteNoteById(id);
+
+            List<Note> noteList = new ArrayList<>(noteService.findAll());
+            for (Note note : noteList) {
+                if (note.getId().equals(id)) {
+                    noteService.remove(note);
+                }
+            }
 
             resp.sendRedirect("/allNotes.jsp");
 
