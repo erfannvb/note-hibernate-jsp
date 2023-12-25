6581443c71f0002a6ee6257a4a604c5390b3893e
Erfan Navab <erfannavab@gmail.com>
2023-12-25 13:20:50 +0330

    implemented base entity which contains the id attribute

---
+++ src/main/java/org/example/notehibernatejsp/base/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package org.example.notehibernatejsp.base.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity<ID extends Serializable> implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+}
