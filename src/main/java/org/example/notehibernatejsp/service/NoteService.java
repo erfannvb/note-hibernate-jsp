f91cd2bc691c306039ca430220845d11b01f3780
Erfan Navab <erfannavab@gmail.com>
2023-12-25 13:38:14 +0330

    added note service layer

---
+++ src/main/java/org/example/notehibernatejsp/service/NoteService.java
@@ -0,0 +1,7 @@
+package org.example.notehibernatejsp.service;
+
+import org.example.notehibernatejsp.base.service.BaseService;
+import org.example.notehibernatejsp.entity.Note;
+
+public interface NoteService extends BaseService<Long, Note> {
+}
---
+++ src/main/java/org/example/notehibernatejsp/service/impl/NoteServiceImpl.java
@@ -0,0 +1,17 @@
+package org.example.notehibernatejsp.service.impl;
+
+import org.example.notehibernatejsp.base.service.impl.BaseServiceImpl;
+import org.example.notehibernatejsp.entity.Note;
+import org.example.notehibernatejsp.repository.NoteRepository;
+import org.example.notehibernatejsp.service.NoteService;
+import org.hibernate.Session;
+
+public class NoteServiceImpl extends BaseServiceImpl<Long, Note, NoteRepository> implements NoteService {
+
+    protected final Session session;
+
+    public NoteServiceImpl(Session session, NoteRepository repository) {
+        super(session, repository);
+        this.session = session;
+    }
+}
