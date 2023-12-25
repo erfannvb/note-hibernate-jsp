a48ce5d00bec1338730d6d70bf7444c5b0aa3c71
Erfan Navab <erfannavab@gmail.com>
2023-12-25 13:21:09 +0330

    implemented base repository

---
+++ src/main/java/org/example/notehibernatejsp/base/repository/impl/BaseRepositoryImpl.java
@@ -0,0 +1,45 @@
+package org.example.notehibernatejsp.base.repository.impl;
+
+import lombok.AllArgsConstructor;
+import org.example.notehibernatejsp.base.entity.BaseEntity;
+import org.example.notehibernatejsp.base.repository.BaseRepository;
+import org.hibernate.Session;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+@AllArgsConstructor
+public abstract class BaseRepositoryImpl<ID extends Serializable, Entity extends BaseEntity<ID>>
+        implements BaseRepository<ID, Entity> {
+
+    protected final Session session;
+
+    public abstract Class<Entity> getEntityClass();
+
+    @Override
+    public void save(Entity entity) {
+        session.persist(entity);
+    }
+
+    @Override
+    public void update(Entity entity) {
+        if (findById(entity.getId()).isPresent()) session.merge(entity);
+    }
+
+    @Override
+    public void remove(Entity entity) {
+        session.remove(entity);
+    }
+
+    @Override
+    public Optional<Entity> findById(ID id) {
+        return Optional.ofNullable(session.find(getEntityClass(), id));
+    }
+
+    @Override
+    public Collection<Entity> findAll() {
+        return session.createQuery("from " + getEntityClass().getSimpleName(), getEntityClass())
+                .getResultList();
+    }
+}
